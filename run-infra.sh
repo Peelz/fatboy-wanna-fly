03328b1728d2a402c526e35b5eb3d8af39164b94
Anucha Mai <anucha.mai@truedigital.com>
2025-04-02 17:16:21 +0700

    edit example name

--- run-infra.sh
+++ run-infra.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 [ -z "$1" ] && {
-  echo "\$1 is empty"
+  echo "prefix \$1 is empty"
   exit 1
 }
-
 ress=(
   sa
   directus_secret
@@ -13,7 +12,7 @@ ress=(
 )
 
 for res in "${ress[@]}"; do
-  pulumi stack init mordee-$1-dev-${res} \
+  pulumi stack init $1-directus-dev-${res} \
     --secrets-provider gcpkms://projects/tdg-dh-truehealth-core-nonprod/locations/asia-southeast1/keyRings/pulumi-stack/cryptoKeys/default-key
   pulumi config set gcp:project tdg-dh-truehealth-core-nonprod
 done
---
+++ up-infra.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+[ -z "$1" ] && {
+  echo "prefix \$1 is empty"
+  exit 1
+}
+ress=(
+  sa
+  directus_secret
+  db_secret
+  eso
+)
+
+for res in "${ress[@]}"; do
+  pulumi up -r -s $1-directus-dev-${res} 
+done
