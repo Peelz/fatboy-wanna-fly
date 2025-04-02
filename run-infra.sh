0f1c45ebb4bc26843703de6439b3810c2be3664d
Anucha Mai <anucha.mai@truedigital.com>
2025-04-02 17:06:35 +0700

    add example pulumi file

--- run-infra.sh
+++ run-infra.sh
@@ -6,13 +6,14 @@
 }
 
 ress=(
+  sa
+  directus_secret
   db_secret
   eso
-  sa
 )
 
 for res in "${ress[@]}"; do
-  pulumi stack init mordee-$1-dev-{res} \
+  pulumi stack init mordee-$1-dev-${res} \
     --secrets-provider gcpkms://projects/tdg-dh-truehealth-core-nonprod/locations/asia-southeast1/keyRings/pulumi-stack/cryptoKeys/default-key
   pulumi config set gcp:project tdg-dh-truehealth-core-nonprod
 done
