31819bfa101678e953ff896d32577de633df95bf
Anucha Mai <anucha.mai@truedigital.com>
2025-04-08 16:22:43 +0700

    remove non use

--- projects/directus/db/entrypoint.sh
+++ projects/directus/db/entrypoint.sh
@@ -4,9 +4,11 @@
 # if [ "$ret_val" -ne 0 ]; then
 #   exit 1
 # fi
+#
+basedir=$(dirname $0)
 
 ./sql/create-users.sql.sh
 ./sql/create-db.sh
 ./sql/grant.sql.sh
 
-run-pgsql-script.sh $DB_NAME $DB_DIRECTUS_USER $DB_ADMIN_USER_PASS "$(pwd)/sql/create-schema.sql"
+# run-pgsql-script.sh $DB_NAME $DB_DIRECTUS_USER $DB_ADMIN_USER_PASS "$dirname/sql/create-schema.sql"
